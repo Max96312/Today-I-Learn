9084acb0aa0d160eaf3420ad6109be441f3f4b55
Max96312 <k0mp4ny16@gmail.com>
2025-02-17 23:37:21 +0900

    sec03: JavaScript 심화

---
+++ React/ONEBITE-REACT/section02/chapter07.js
@@ -0,0 +1,30 @@
+// 6가지의 요소 조작 메서드
+
+// 1. push
+// 배열의 맨 뒤에 새로운 요소를 추가하는 메서드
+let arr1 = [1, 2, 3];
+const newLength = arr1.push(4, 5, 6, 7);
+
+// console.log(arr1);
+// console.log(newLength);
+
+// 2. pop
+// 배열의 맨 뒤에 있는 요소를 제거하고, 반환
+let arr2 = [1, 2, 3];
+const poppedItem = arr2.pop();
+
+// console.log(poppedItem);
+// console.log(arr2);
+
+// 3. shift
+// 배열의 맨 앞에 있는 요소를 제거, 반환
+let arr3 = [1, 2, 3];
+const shiftedItem = arr3.shift();
+console.log(shiftedItem, arr3);
+
+// 4. unshift
+// 배열의 맨 앞에 새로운 요소를 추가하는 메서드
+let arr4 = [1, 2, 3];
+const newLength2 = arr4.unshift(0);
+// console.log(arr4);
+console.log(newLength2, arr4);
