09b33b42a9c212d0d86278b7da1d712fa11617a4
Max96312 <k0mp4ny16@gmail.com>
2025-09-15 23:16:15 +0900

    Tip04: 템플릿 리터럴로 변수를 읽을 수 있는 문자열로 변환하라

---
+++ JavaScript/JavaScript 코딩의 기술/sec01/04.js
@@ -0,0 +1,26 @@
+function generateLink(image, width) {
+    const widthInt = parseInt(width, 10);
+    return 'https://' + getProvider() + '/' + image + '?width=' + widthInt;
+}
+
+function greet(name) {
+    return `Hi, ${name}`;
+}
+greet('Leo');
+'Hi, Leo';
+
+function yell(name) {
+    return `HI, ${name.toUpperCase()}!`;
+}
+yell('Pankaj');
+'HI, PANKAJ!';
+
+function leapYearConverter(age) {
+    return `윤년에 태어났다면 ${Math.floor(age / 4)}살이야.`;
+}
+leapYearConverter(34);
+// "윤년에 태어났다면 8살이야."
+
+function generateLink(image, width) {
+    return `https://${getProvider()}/${image}?width=${parseInt(width, 10)}`;
+}
